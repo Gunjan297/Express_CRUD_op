5e8696144d539d447ac66d7b1ed2492d6d690ea5
Gunjan Jain <mgpsgunjan6166@gmail.com>
2025-06-06 15:43:30 +0530

    update files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,8 +11,26 @@ app.use(express.json());
 let teaData = [];
 let nextId = 1;
 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 //add a new tea
 app.post("/teas", (req, res) => {
+  logger.warn("A post request was made to add a tea");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
